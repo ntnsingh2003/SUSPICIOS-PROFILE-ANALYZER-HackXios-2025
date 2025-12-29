de13d80dfa73688607e8cfba7ac6064ad90cf31a
nitinsingh <ntnsingh2003@gmail.com>
2025-12-30 00:46:27 +0530

    ele

--- main.py
+++ main.py
@@ -337,3 +337,6 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
+
+# For gunicorn deployment
+application = app
